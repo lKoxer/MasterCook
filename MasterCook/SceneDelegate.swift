4e23884bc8b6697ef327bf1c71554ab79640948f
Alex <102607576+lKoxer@users.noreply.github.com>
2023-02-28 14:57:42 +0100

    Корректировка scenedelegate

--- MasterCook/SceneDelegate.swift
+++ MasterCook/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let viewController = StartView()
+        let viewController = ViewController()
         window.rootViewController = viewController
         self.window = window
 
@@ -53,7 +53,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
 
         // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
