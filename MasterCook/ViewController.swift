9a0b2882b73b3be4d7dfe77e915e9828404c0aff
leogoba <leogobamusic@yandex.ru>
2023-03-01 19:22:20 +0400

    NetworkData and NetworkManager и проверка сетевого запроса

--- MasterCook/ViewController.swift
+++ MasterCook/ViewController.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var qwe = NetworkManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //test
+        view.backgroundColor = .cyan
+        qwe.getAllRecipes()
     }
-
-
 }
 
