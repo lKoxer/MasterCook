0ae45d4316686007dada960795bce295fe1c8e1b
leogoba <leogobamusic@yandex.ru>
2023-03-06 00:22:18 +0400

    Work with UI

--- MasterCook/ViewController.swift
+++ MasterCook/ViewController.swift
@@ -15,6 +15,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .cyan
         qwe.getAllRecipes()
+        initialize()
     }
 }
 
+extension ViewController {
+    func initialize() {
+        let label = UILabel()
+        label.text = "Welcome!"
+        
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        let topAnchor = label.topAnchor.constraint(equalTo: view.topAnchor, constant: 100)
+        let bottonAnchor = label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 100)
+        let leftAnchor = label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10)
+        let rightAnchor = label.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 10)
+        NSLayoutConstraint.activate([topAnchor, bottonAnchor, leftAnchor, rightAnchor])
+    }
+}
